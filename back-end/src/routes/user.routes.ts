da3bdffd018862d3311f071856e707c189522cdb
Alejandro Canals <129771462+AlejandroCanals@users.noreply.github.com>
2024-12-30 17:31:50 +0100

    auth basico terminado

--- back-end/src/routes/user.routes.ts
+++ back-end/src/routes/user.routes.ts
@@ -3,7 +3,6 @@ import UserController from '../controllers/user.controller';
 
 const router = Router();
 
-
 router.post('/', UserController.createUser);
 router.get('/:id', UserController.getUserById);
 router.get('/', UserController.getAllUsers);
